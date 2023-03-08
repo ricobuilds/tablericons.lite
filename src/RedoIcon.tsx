dca714ad1824e6ba8e16dd2d5a309e2259d48643
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-08 21:15:56 +0000

    c: added Redo and Undo

---
+++ src/RedoIcon.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const RedoIcon = () => {
+    return (
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"/><path d="M9 13 5 9l4-4M5 9h11a4 4 0 0 1 0 8h-1"/></svg>
+    )
+}
