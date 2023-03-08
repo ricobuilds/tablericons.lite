1ad662470421d4809f4f4067950e30187555b731
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-08 22:02:57 +0000

    c: HeartIcon

---
+++ src/HeartIcon.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const HeartIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"/><path d="M19.5 12.572 12 20l-7.5-7.428A5 5 0 1 1 12 6.006a5 5 0 1 1 7.5 6.572"/></svg>
+    )
+}
