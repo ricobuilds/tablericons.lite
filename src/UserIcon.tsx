459588b336c1994671dd98e7d563b69a252713e2
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-18 10:32:28 +0000

    added `UserIcon`

---
+++ src/UserIcon.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const UserIcon = () => {
+
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24"><path stroke="none" d="M0 0h24v24H0z"/><path d="M8 7a4 4 0 1 0 8 0 4 4 0 0 0-8 0M6 21v-2a4 4 0 0 1 4-4h4a4 4 0 0 1 4 4v2"/></svg>
+  )
+}
