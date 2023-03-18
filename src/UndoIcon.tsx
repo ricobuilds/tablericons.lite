c988bbe0ed89df1a18fb456b7e62510369d1a0c8
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-18 10:15:02 +0000

    updated `UndoIcon` to 20x20

--- src/UndoIcon.tsx
+++ src/UndoIcon.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const UndoIcon = () => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor"><path stroke="none" d="M0 0h24v24H0z"/><path d="M22 4.01c-1 .49-1.98.689-3 .99-1.121-1.265-2.783-1.335-4.38-.737S11.977 6.323 12 8v1c-3.245.083-6.135-1.395-8-4 0 0-4.182 7.433 4 11-1.872 1.247-3.739 2.088-6 2 3.308 1.803 6.913 2.423 10.034 1.517 3.58-1.04 6.522-3.723 7.651-7.742a13.84 13.84 0 0 0 .497-3.753c0-.249 1.51-2.772 1.818-4.013z"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24"><path stroke="none" d="M0 0h24v24H0z"/><path d="M9 13 5 9l4-4M5 9h11a4 4 0 0 1 0 8h-1"/></svg>
     )
 }
