25b57851f35e144631200ed3aa621dd3fa7c668d
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-18 09:55:14 +0000

    updated `ChatIcon` to 20x20

--- src/ChatIcon.tsx
+++ src/ChatIcon.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const ChatIcon = () => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z"/><path d="m3 20 1.3-3.9A9 8 0 1 1 7.7 19L3 20M12 12v.01M8 12v.01M16 12v.01"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24"><path stroke="none" d="M0 0h24v24H0z"/><path d="M4 21V8a3 3 0 0 1 3-3h10a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H8l-4 4M12 11v.01M8 11v.01M16 11v.01"/></svg>
     )
 }
