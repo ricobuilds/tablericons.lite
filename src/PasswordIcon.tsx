32519f8f994f9e6e996ae5ddec1041c997086944
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-18 10:06:08 +0000

    added `PasswordIcon` at 20x20

---
+++ src/PasswordIcon.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+export const PasswordIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24"><path stroke="none" d="M0 0h24v24H0z"/><path d="M12 10v4M10 13l4-2M10 11l4 2M5 10v4M3 13l4-2M3 11l4 2M19 10v4M17 13l4-2M17 11l4 2"/></svg>
+  )
+}
