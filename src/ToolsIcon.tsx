bd5ff0d886235bc453b955b71f6750087be1ce53
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-18 09:49:53 +0000

    added `ToolsIcon`

---
+++ src/ToolsIcon.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+export const ToolsIcons = () => {
+
+  return (
+<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke="none" d="M0 0h24v24H0z"/><path d="M3 21h4L20 8a1.5 1.5 0 0 0-4-4L3 17v4M14.5 5.5l4 4"/><path d="M12 8 7 3 3 7l5 5M7 8 5.5 9.5M16 12l5 5-4 4-5-5M16 17l-1.5 1.5"/></svg>
+  )
+}
