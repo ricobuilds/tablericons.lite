5cb5239ee4c14358ec31c69ba3906e20b65a38ac
Enric Trenric_trillo <enric_trillo@outlook.com>
2023-03-20 16:04:44 +0000

    added NpmIcon

---
+++ src/NpmIcon.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const NpmIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24"><path stroke="none" d="M0 0h24v24H0z"/><path d="M1 8h22v7H11v2H7v-2H1zM7 8v7M14 8v7M17 11v4M4 11v4M11 11v1M20 11v4"/></svg>
+  )
+}
